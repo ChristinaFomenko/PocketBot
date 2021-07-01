e9f7ce4df2758317510207c5ed6d2144cde023d5
Christina <kris.inferno@gmail.com>
2021-07-01 17:39:14 +0300

    const

--- pocket.go
+++ pocket.go
@@ -1,1 +1,7 @@
 package pocket
+
+const (
+	host          = "https://getpocket.com/v3"
+	authorizedUrl = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s"
+	endpointAdd   = "/add"
+)
