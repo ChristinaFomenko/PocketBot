bbf728e1a25678c40e4b937a3ac931a9a9f660cd
Christina <kris.inferno@gmail.com>
2021-07-02 11:05:06 +0300

    basic structure

--- pocket.go
+++
@@ -1,7 +0,0 @@
-package pocket
-
-const (
-	host          = "https://getpocket.com/v3"
-	authorizedUrl = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s"
-	endpointAdd   = "/add"
-)
