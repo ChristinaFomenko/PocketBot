ab477617447c7db6d4e413fbabd7d845511a75e4
Christina <kris.inferno@gmail.com>
2021-07-02 11:31:06 +0300

    split func

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/KrisInferno/PocketBot/pkg/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 )
@@ -13,24 +14,9 @@ func main() {
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		//msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
 	}
 
 }
